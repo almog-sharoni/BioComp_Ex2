0e411b6ae0a46324c9a0c4471b58ee1d592ca4c3
chencg10 <ccoheng@gmail.com>
2023-06-12 18:54:00 +0300

    update

--- BioComp_ex2_p2.py
+++ BioComp_ex2_p2.py
@@ -1,4 +1,4 @@
-import BioComp_ex2_p
+#import BioComp_ex2_p
 import itertools
 import networkx as nx
 
@@ -13,22 +13,29 @@ def main():
     # TODO:  read edges from file
     # create an empty directed graph
     graph = nx.DiGraph()
-    while True:
-        with open('edges.txt', 'r') as f:
-            edge = f.read()
+
+
+    with open('edges.txt', 'r') as f:
+        while True:
+            edge = f.readline()
             # check if file is empty or end of file is reached
             if not edge:
                 break
-        # TODO: the format of the line in file is 'u v',
-        # we want to obtain u and v as integers
-        u, v = edge.split()
-        u = int(u)
-        v = int(v)
-        # insert the edge to the graph
-        graph.add_edge(u, v)
+            # TODO: the format of the line in file is 'u v',
+            # we want to obtain u and v as integers
+            u, v = edge.split()
+            u = int(u)
+            v = int(v)
+            # insert the edge to the graph
+            graph.add_edge(u, v)
+        # close the file
+        f.close()
+
+
 
     # print the edges
     print(graph.edges())
 
 
-main()
+if __name__ == "__main__":
+    main()
